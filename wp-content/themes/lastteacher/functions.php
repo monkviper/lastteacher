63a05c1381f2cb98fd5bd7e512c94ae2b6853215
Mridul Aggarwal <mridul_aggarwal@yahoo.co.in>
2014-11-09 04:15:16 +0530

    adding basic files needed for the theme

---
+++ wp-content/themes/lastteacher/functions.php
@@ -0,0 +1,39 @@
+<?php
+
+/* Last Teacher Theme Version */
+define( 'LT_VERSION', '1.0' );
+
+/* Last Teacher Theme Directory Paths */
+define( 'LT_THEME_DIR', get_template_directory() );
+define( 'LT_EXT_DIR', LT_THEME_DIR . '/ext' );
+define( 'LT_INC_DIR', LT_THEME_DIR . '/inc' );
+define( 'LT_JS_DIR', LT_THEME_DIR . '/js' );
+
+/**
+ * Set the content width based on the theme's design and stylesheet.
+ */
+if( !isset( $content_width ) ) {
+	$content_width = 640; /* pixels */
+}
+
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which
+ * runs before the init hook. The init hook is too late for some features, such
+ * as indicating support for post thumbnails.
+ */
+function lt_setup() {
+}
+
+add_action( 'after_setup_theme', 'lt_setup' );
+
+/**
+ * Enqueue scripts and styles.
+ */
+function lt_scripts() {
+	wp_enqueue_style( 'lt-style', get_stylesheet_uri() );
+}
+
+add_action( 'wp_enqueue_scripts', 'lt_scripts' );
+

dd93edb34f8e9155b3b1dd1e4ac30caadd6de939
Mridul Aggarwal <mridul_aggarwal@yahoo.co.in>
2014-11-29 21:06:03 +0530

    adding a plugin for tweaks and some code that isn't suited for theme

---
+++ wp-content/plugins/lastteacher/lastteacher.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Last Teacher
+Plugin URI: http://lastteacher.com/
+Description: This is a custom plugin for Last Teacher website theme for stuff that isn't suitable in the theme functions file.
+Author: Mridul Aggarwal
+Version: 1.0
+Author URI: http://plusya.com/mridul89
+*/
+
+

b6e92e0511d0c0e792ca79b7d16af6cef62e9898
Mridul Aggarwal <mridul_aggarwal@yahoo.co.in>
2014-12-01 04:23:03 +0530

    changes to the class file

--- wp-content/themes/lastteacher/inc/class-Exam.php
+++ wp-content/themes/lastteacher/inc/class-Exam.php
@@ -12,105 +12,124 @@ class LT_Exam {
 	 *
 	 * @var int
 	 */
-	private $ID;
+	public $ID;
 	/**
 	 * The name of the exam to be used as the title
 	 *
 	 * @var string
 	 */
-	private $name;
+	public $name;
 	/**
 	 * Total maximum for the marks. The percentage is calculated using this.
 	 *
 	 * @var int
 	 */
-	private $total_marks;
+	public $total_marks;
 	/**
 	 * The marks required for passing.
 	 *
 	 * @var int
 	 */
-	private $pass_marks;
+	public $pass_marks;
 	/**
 	 * Total number of questions in this exam. Exam will be considered invalid without that many questions initialised.
 	 *
 	 * @var int
 	 */
-	private $total_questions;
+	public $total_questions;
 	/**
 	 * Marks awarded for every correct answer
 	 *
 	 * @var int
 	 */
-	private $correct_answer_marks;
+	public $correct_answer_marks;
 	/**
 	 * Negative marks that are taken away for every wrong answer
 	 *
 	 * @var int
 	 */
-	private $wrong_answer_marks;
-	/**
-	 * Weather this exam has already been attempted
-	 *
-	 * @var bool
-	 */
-	private $completed;
-	/**
-	 * If this exam has been completed, how many marks were achieved.
-	 *
-	 * @var int
-	 */
-	private $marks_achieved;
+	public $wrong_answer_marks;
 	/**
 	 * The author who published this exam
 	 *
 	 * @var string
 	 */
-	private $author;
-	/**
-	 * The time at which exam was started
-	 *
-	 * @var int
-	 */
-	private $time_started;
-	/**
-	 * The time at which exam was ended
-	 *
-	 * @var int
-	 */
-	private $time_ended;
-	/**
-	 * The user who wrote this exam
-	 *
-	 * @var WP_User
-	 */
-	private $user;
+	public $author;
 	/**
 	 * An array of all the questions for this exam
 	 *
 	 * @var array
 	 */
-	private $questions;
+	public $questions;
 
-	function __get( $name ) {
-		if( isset( $this->$name ) ) {
-			return $this->$name;
-		} else {
-			return null;
+	function __construct( $ID = null ) {
+		if( $ID ) {
+			$this->loadById( $ID );
+		}
+
+		$this->ID = $ID;
+	}
+
+	function reset() {
+		$this->ID = null;
+		$this->name = '';
+		$this->total_questions = 0;
+		$this->total_marks = 0;
+		$this->pass_marks = 0;
+		$this->correct_answer_marks = 0;
+		$this->wrong_answer_marks = 0;
+		$this->author = null;
+		$this->questions = array();
+	}
+
+	function loadById( $id ) {
+		$this->reset();
+
+		/** @var WPDB $wpdb */
+		global $wpdb;
+
+		$tablename = $wpdb->prefix . "exam";
+
+		$row = $wpdb->get_row( "SELECT * FROM $tablename WHERE ID = $id" );
+
+		if( is_object( $row ) ) {
+			$this->ID = $row->ID;
+			$this->name = $row->name;
+			$this->total_questions = $row->total_questions;
+			$this->pass_marks = $row->pass_marks;
+			$this->correct_answer_marks = $row->correct_answer_marks;
+			$this->wrong_answer_marks = $row->wrong_answer_marks;
+			$this->total_marks = $this->total_questions * $this->correct_answer_marks;
 		}
 	}
 
-	function __set( $name, $value ) {
-		if( isset( $this->$name ) ) {
-			$this->$name = $value;
+	function save() {
+		if( $this->ID ) { // existing record
+			/** @var WPDB $wpdb */
+			global $wpdb;
+
+			$tablename = $wpdb->prefix . "exam";
+			$newdata = array(
+					'name'                 => $this->name,
+					'total_questions'      => $this->total_questions,
+					'pass_marks'           => $this->pass_marks,
+					'correct_answer_marks' => $this->correct_answer_marks,
+					'wrong_answer_marks'   => $this->wrong_answer_marks,
+			);
+			$format = array( '%s', '%d', '%d', '%d', '%d' );
+			$where = array( 'ID' => $this->ID );
+			$where_format = array( '%d' );
+
+			$exist = $wpdb->get_var( "SELECT COUNT(*) FROM $tablename WHERE ID = {$this->ID}" );
+
+			if( $exist ) {
+				return $wpdb->update( $tablename, $newdata, $where, $format, $where_format );
+			} else {
+				$newdata['ID'] = $this->ID;
+				return $wpdb->insert( $tablename, $newdata, $format );
+			}
+		} else { // new record
+			// Currently we are using the post id to save exams so if the id is not present we can't save it
 		}
-	}
-
-	function __isset( $name ) {
-		return isset( $this->$name );
-	}
-
-	function __unset( $name ) {
-		$this->$name = null;
 	}
 }

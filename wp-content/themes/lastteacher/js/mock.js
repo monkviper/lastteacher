089f5d2cc3b8b2e19e02ea9ce885bd2bd45154be
Mridul Aggarwal <mridul_aggarwal@yahoo.co.in>
2015-01-18 19:27:24 +0530

    adding a function for ajax

--- wp-content/themes/lastteacher/js/mock.js
+++ wp-content/themes/lastteacher/js/mock.js
@@ -1,4 +1,18 @@
 (function($, window) {
+	var _ajax_url = lt_ajax_url;
+	var _ajax_nonce = lt_ajax_nonce;
+	var ajax = function(action, params, callback) {
+		params = params || {};
+		if(!$.isObject(params)) {
+			params = {};
+		}
+		params.action = action;
+		params._ajax_nonce = _ajax_nonce;
+		$.post(_ajax_url, params, function(response) {
+			callback.call(this, response);
+		}, 'json');
+	};
+
 	window.initiate_mock = function(params) {
 		console.log(params);
 	};

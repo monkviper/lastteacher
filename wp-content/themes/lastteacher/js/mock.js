abd302b6a8ff720fccc6cbc32d3131ebdf359479
Mridul Aggarwal <mridul_aggarwal@yahoo.co.in>
2015-01-18 20:43:39 +0530

    adding code to initiate a new mock and setup the environment for the user

--- wp-content/themes/lastteacher/js/mock.js
+++ wp-content/themes/lastteacher/js/mock.js
@@ -9,8 +9,31 @@
 		params.action = action;
 		params._ajax_nonce = _ajax_nonce;
 		$.post(_ajax_url, params, function(response) {
-			callback.call(this, response);
+			callback.apply(this, arguments);
 		}, 'json');
+	};
+
+	var container = $('#mock');
+	var placeholder = $('#placeholder');
+	var main = $('#main');
+	var paused = $('#paused');
+	var instructions = $('#instructions');
+	var finished = $('#finished');
+
+	var new_mock_session = function(mock_id) {
+		container.addClass('preparing');
+		ajax('register_new_mock', {mock: mock_id}, function(response) {
+			if(response.error) {
+				alert(response.message);
+				if('undefined' != typeof response.redirect && response.redirect) {
+					window.location.href = response.redirect;
+				} else if('undefined' != typeof response.reload && response.reload) {
+					window.location.reload(true);
+				}
+			} else {
+				//@todo new mock session created, and loaded, save the info and show the appropriate forms to user
+			}
+		});
 	};
 
 	window.initiate_mock = function(params) {

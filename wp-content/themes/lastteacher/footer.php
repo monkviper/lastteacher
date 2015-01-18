ab6109a83d08e318ba05af5c08bc55032e5ac628
Mridul Aggarwal <mridul_aggarwal@yahoo.co.in>
2015-01-18 19:27:02 +0530

    adding variables to help with ajax

--- wp-content/themes/lastteacher/footer.php
+++ wp-content/themes/lastteacher/footer.php
@@ -1,4 +1,8 @@
 
+<script type="text/javascript">
+	var lt_ajax_url = '<?php admin_url('admin-ajax.php'); ?>';
+	var lt_ajax_nonce = '<?php echo wp_create_nonce('_ajax'); ?>';
+</script>
 <?php wp_footer(); ?>
 
 </body>

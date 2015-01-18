79701289100cfbcb4b2a57a6e34c7c5089029412
Mridul Aggarwal <mridul_aggarwal@yahoo.co.in>
2015-01-18 21:58:29 +0530

    error checking in the new mock functions

--- wp-content/themes/lastteacher/inc/ajax.php
+++ wp-content/themes/lastteacher/inc/ajax.php
@@ -23,8 +23,32 @@ class LT_AJAX {
 	}
 
 	function register_new_mock( $params ) {
-		$mock_id = $params['mock'];
-		$mock = new LT_Mock($mock_id);
+		$mock_id = intval( $params['mock'] );
+		$mock = new LT_Mock( $mock_id );
+		if( !$mock_id || $mock_id != $mock->id ) {
+			return array( 'error' => 1, 'message' => 'Invalid Mock ID', 'redirect' => home_url( '/' ) );
+		}
+
+		$user_id = get_current_user_id();
+		if( !$user_id ) {
+			return array( 'error' => 1, 'message' => 'Invalid User', 'redirect' => home_url( '/' ) );
+		}
+
+		//@todo check if the current user has permissions to access this mock
+		if( false ) {
+			return array( 'error' => 1, 'message' => 'You haven\'t purchased this exam.', 'redirect' => home_url( '/' ) );
+		}
+
+		global $wpdb;
+		$table_name = $wpdb->prefix . 'exams_records';
+
+		$existing = $wpdb->get_var( "SELECT COUNT(*) FROM $table_name WHERE mock = $mock_id AND user = $user_id" );
+		if($existing) {
+			return array( 'error' => 0, 'message' => 'This Exam is already running in another window', 'status' => 'unavailable' );
+		}
+
+		// If we reached here, we can successfully start this exam
+		//@todo write the code to start the exam, create it's record and notify js to start it
 	}
 }
 

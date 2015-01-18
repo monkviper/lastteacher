2dce11b34b6d42294204285adbadbb10bad95b71
Mridul Aggarwal <mridul_aggarwal@yahoo.co.in>
2015-01-19 01:18:59 +0530

    adding output

--- wp-content/themes/lastteacher/footer.php
+++ wp-content/themes/lastteacher/footer.php
@@ -1,6 +1,6 @@
 
 <script type="text/javascript">
-	var lt_ajax_url = '<?php admin_url('admin-ajax.php'); ?>';
+	var lt_ajax_url = '<?php echo admin_url('admin-ajax.php'); ?>';
 	var lt_ajax_nonce = '<?php echo wp_create_nonce('_ajax'); ?>';
 </script>
 <?php wp_footer(); ?>
